16d38e41c1b860f86e4d7fb1f96163785fdb69fc
johnnybee4e <jon.rosado42@gmail.com>
2018-04-24 18:06:23 -0400

    finish sequelize essentials... kind of

--- models/index.js
+++ models/index.js
@@ -1,11 +1,59 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false
+});
 
 const Page = db.define('page', {
-
+  title: {
+    type: Sequelize.STRING,
+    validate: {
+      allowNull: false
+    }
+  },
+  urlTitle:{
+    type: Sequelize.STRING,
+    validate: {
+      allowNull: false,
+      isUrl: true
+    }
+  },
+  content: {
+    type: Sequelize.TEXT,
+    validate: {
+      allowNull: false
+    }
+  },
+  status: {
+    type: Sequelize.ENUM('open','close')
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  },
+  urlGetterMethods: {
+    urlRoute() {
+      return '/wiki/' + this.urlTitle
+    }
+  }
 });
 
 const User = db.define('user', {
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+    validate: {
+      allowNull: false
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      allowNull: false,
+      isEmail: true
+    }
+  },
 });
+
+
+module.exports = {
+  db, Page, User
+};
--- wikistack.js
+++ wikistack.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const path = require('path');
 const app = express();
-
+const models = require('./models');
 app.use(morgan('dev'));
 
 const env = nunjucks.configure('views', {noCache: true});
@@ -12,10 +12,19 @@ app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 app.use(express.static(path.join(__dirname, '/public')));
 
+
+
 app.get('/', (req,res,next) => {
   res.send('./index.html');
 });
 
-app.listen(3000, () => {
-  console.log('Running on port 3000!');
-});
+models.db.sync()
+.then(function() {
+  console.log('All table created!');
+  app.listen(3000, () => {
+    console.log('Running on port 3000!');
+  });
+})
+.catch(console.error.bind(console));
+
+
